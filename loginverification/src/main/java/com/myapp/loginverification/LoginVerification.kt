c78a4b247845f4213feba8432bba7a5de977dc54
TensePanic <ala0712111998@gmail.com>
2023-11-20 17:48:03 +0100

    Add new user credentials

--- loginverification/src/main/java/com/myapp/loginverification/LoginVerification.kt
+++ loginverification/src/main/java/com/myapp/loginverification/LoginVerification.kt
@@ -2,7 +2,8 @@ package com.myapp.loginverification
 
 class LoginVerification {
     fun verify(email: String, password: String): Boolean {
-        return email == "ala07@gmail.com" &&
-                password == "test123"
+        return (email == "ala07@gmail.com" &&
+                password == "test123") ||
+                (email == "test@user.com" && password == "user123")
     }
 }
